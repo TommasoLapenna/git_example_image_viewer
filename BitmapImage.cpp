029cde6ea3f53ff907a6ce211252271de6f9e837
TommasoLapenna <tommaso.lapenna@stud.unifi.it>
2023-03-27 17:42:13 +0200

    Added save function

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -15,6 +15,10 @@ bool BitmapImage::load(std::string name) {
     return false;
 }
 
+bool BitmapImage::save(std::string name) {
+    return true;
+}
+
 BitmapImage::~BitmapImage() {
     delete[] buffer;
 }
